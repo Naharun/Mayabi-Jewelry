7c23ce684a0d38ce4955ca40f3b1f91efcb76456
Naharun <naharunsraboni@gmail.com>
2023-10-08 16:01:05 +0600

    Created Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
