bb8e10d2cd46f6c8e7d625850ce80d7a427a2a23
Naharun <naharunsraboni@gmail.com>
2023-10-12 02:54:10 +0600

    final commit

--- src/pages/MyJewelry/MyJewelry.jsx
+++ src/pages/MyJewelry/MyJewelry.jsx
@@ -2,7 +2,7 @@
 const MyJewelry = () => {
     return (
         <div>
-            <h2 className=" mx-auto text-center font-bold p-8 bg-slate-900 text-3xl text-white">My All Jewelry  Are Here....!!!</h2>
+            <h2 className="italic mx-auto text-center font-bold p-8 bg-slate-900 text-3xl text-white">My All Jewelry  Are Here....!!!</h2>
         </div>
     );
 };
